ba6b68ff338cfefd074c8d5f954f6eeca292e0f2
Viccodes <ayeniv69@gmail.com>
2025-10-16 15:27:16 +0100

    properly configured the swagger ui settings to accept jwt token

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -84,6 +84,19 @@ REST_FRAMEWORK = {
     )
 }
 
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header',
+            'description': 'JWT Authorization header using the Bearer scheme. Example: "Bearer <your_token>"',
+        }
+    },
+    'USE_SESSION_AUTH': False,
+}
+
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
